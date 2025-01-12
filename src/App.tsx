5098aa596a1cdd6fd693f71f0dc4059c6b3b06ae
Kisan <kisandas13@gmail.com>
2025-01-12 22:48:23 +0530

    test shad cn btn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
   return (
     <>
       <h1>Welcome to elib-dashzpoard</h1>
+      <Button variant={"destructive"}>Click Me bro</Button>
     </>
   );
 }
