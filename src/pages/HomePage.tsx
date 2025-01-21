bd1dc23526d83aea5f3fba58976edd8785b01750
Kisan <kisandas13@gmail.com>
2025-01-21 18:34:54 +0530

    update home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -108,15 +108,9 @@ const HomePage = () => {
                     <TableHeader>
                       <TableRow>
                         <TableHead>Customer</TableHead>
-                        <TableHead className="hidden xl:table-column">
-                          Type
-                        </TableHead>
-                        <TableHead className="hidden xl:table-column">
-                          Status
-                        </TableHead>
-                        <TableHead className="hidden xl:table-column">
-                          Date
-                        </TableHead>
+                        <TableHead>Type</TableHead>
+                        <TableHead>Status</TableHead>
+                        <TableHead>Date</TableHead>
                         <TableHead className="text-right">Amount</TableHead>
                       </TableRow>
                     </TableHeader>
@@ -124,19 +118,17 @@ const HomePage = () => {
                       <TableRow>
                         <TableCell>
                           <div className="font-medium">Liam Johnson</div>
-                          <div className="hidden text-sm text-muted-foreground md:inline">
+                          <div className=" text-sm text-muted-foreground md:inline">
                             liam@example.com
                           </div>
                         </TableCell>
-                        <TableCell className="hidden xl:table-column">
-                          Sale
-                        </TableCell>
-                        <TableCell className="hidden xl:table-column">
+                        <TableCell>Sale</TableCell>
+                        <TableCell>
                           <Badge className="text-xs" variant="outline">
                             Approved
                           </Badge>
                         </TableCell>
-                        <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
+                        <TableCell className="md:table-cell">
                           2023-06-23
                         </TableCell>
                         <TableCell className="text-right">$250.00</TableCell>
@@ -144,19 +136,17 @@ const HomePage = () => {
                       <TableRow>
                         <TableCell>
                           <div className="font-medium">Olivia Smith</div>
-                          <div className="hidden text-sm text-muted-foreground md:inline">
+                          <div className="text-sm text-muted-foreground md:inline">
                             olivia@example.com
                           </div>
                         </TableCell>
-                        <TableCell className="hidden xl:table-column">
-                          Refund
-                        </TableCell>
-                        <TableCell className="hidden xl:table-column">
+                        <TableCell>Refund</TableCell>
+                        <TableCell>
                           <Badge className="text-xs" variant="outline">
                             Declined
                           </Badge>
                         </TableCell>
-                        <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
+                        <TableCell className="md:table-cell">
                           2023-06-24
                         </TableCell>
                         <TableCell className="text-right">$150.00</TableCell>
@@ -164,19 +154,17 @@ const HomePage = () => {
                       <TableRow>
                         <TableCell>
                           <div className="font-medium">Noah Williams</div>
-                          <div className="hidden text-sm text-muted-foreground md:inline">
+                          <div className="text-sm text-muted-foreground md:inline">
                             noah@example.com
                           </div>
                         </TableCell>
-                        <TableCell className="hidden xl:table-column">
-                          Subscription
-                        </TableCell>
-                        <TableCell className="hidden xl:table-column">
+                        <TableCell>Subscription</TableCell>
+                        <TableCell>
                           <Badge className="text-xs" variant="outline">
                             Approved
                           </Badge>
                         </TableCell>
-                        <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
+                        <TableCell className="md:table-cell">
                           2023-06-25
                         </TableCell>
                         <TableCell className="text-right">$350.00</TableCell>
@@ -184,19 +172,17 @@ const HomePage = () => {
                       <TableRow>
                         <TableCell>
                           <div className="font-medium">Emma Brown</div>
-                          <div className="hidden text-sm text-muted-foreground md:inline">
+                          <div className="text-sm text-muted-foreground md:inline">
                             emma@example.com
                           </div>
                         </TableCell>
-                        <TableCell className="hidden xl:table-column">
-                          Sale
-                        </TableCell>
-                        <TableCell className="hidden xl:table-column">
+                        <TableCell>Sale</TableCell>
+                        <TableCell>
                           <Badge className="text-xs" variant="outline">
                             Approved
                           </Badge>
                         </TableCell>
-                        <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
+                        <TableCell className="md:table-cell">
                           2023-06-26
                         </TableCell>
                         <TableCell className="text-right">$450.00</TableCell>
@@ -204,19 +190,17 @@ const HomePage = () => {
                       <TableRow>
                         <TableCell>
                           <div className="font-medium">Liam Johnson</div>
-                          <div className="hidden text-sm text-muted-foreground md:inline">
+                          <div className="text-sm text-muted-foreground md:inline">
                             liam@example.com
                           </div>
                         </TableCell>
-                        <TableCell className="hidden xl:table-column">
-                          Sale
-                        </TableCell>
-                        <TableCell className="hidden xl:table-column">
+                        <TableCell>Sale</TableCell>
+                        <TableCell>
                           <Badge className="text-xs" variant="outline">
                             Approved
                           </Badge>
                         </TableCell>
-                        <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
+                        <TableCell className="md:table-cell">
                           2023-06-27
                         </TableCell>
                         <TableCell className="text-right">$550.00</TableCell>
