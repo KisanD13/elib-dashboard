2d1fc0c2f800b0255a5b542cdd609ee3c2b1e116
Kisan <kisandas13@gmail.com>
2025-01-25 21:16:27 +0530

    add login functionality

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -9,9 +9,33 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Link } from "react-router-dom";
+import { login } from "@/http/api";
+import { useMutation } from "@tanstack/react-query";
+import { useRef } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const navigate = useNavigate();
+
+  const mutation = useMutation({
+    mutationFn: login,
+    onSuccess: () => {
+      navigate("/");
+    },
+  });
+
+  const handleLoginSubmit = () => {
+    const email = emailRef.current?.value;
+    const password = passwordRef.current?.value;
+
+    if (!email || !password) {
+      return alert("Please enter email and password");
+    }
+    mutation.mutate({ email, password });
+  };
+
   return (
     <section className="flex justify-center items-center h-screen">
       <Card className="w-full max-w-sm">
@@ -30,7 +54,7 @@ const LoginPage = () => {
           <div className="grid gap-2">
             <Label htmlFor="email">Email</Label>
             <Input
-              // ref={emailRef}
+              ref={emailRef}
               id="email"
               type="email"
               placeholder="m@example.com"
@@ -39,18 +63,13 @@ const LoginPage = () => {
           </div>
           <div className="grid gap-2">
             <Label htmlFor="password">Password</Label>
-            <Input
-              // ref={passwordRef}
-              id="password"
-              type="password"
-              required
-            />
+            <Input ref={passwordRef} id="password" type="password" required />
           </div>
         </CardContent>
         <CardFooter>
           <div className="w-full">
             <Button
-              // onClick={handleLoginSubmit}
+              onClick={handleLoginSubmit}
               className="w-full"
               // disabled={mutation.isPending}
             >
