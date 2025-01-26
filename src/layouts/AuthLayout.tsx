1032c850c24c0478314ff6a8afd3c858cca5b739
Kisan <kisandas13@gmail.com>
2025-01-26 17:17:10 +0530

    make route protected

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,10 +1,10 @@
-// import useTokenStore from "@/store";
+import useTokenStore from "@/store";
 import { Navigate, Outlet } from "react-router-dom";
 
 const AuthLayout = () => {
-  //   const token = useTokenStore((state) => state.token);
+  const token = useTokenStore((state) => state.token);
 
-  if (false) {
+  if (token) {
     return <Navigate to={"/dashboard/home"} replace />;
   }
 
